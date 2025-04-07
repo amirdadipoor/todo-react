8b03c2a3ab83bc33235b065e088e056ee6ed4bf0
Amir Dadipoor <amirdadipoor>
2025-04-08 01:47:56 +0330

    modify navbar

--- src/components/app-navbar.jsx
+++ src/components/app-navbar.jsx
@@ -9,7 +9,7 @@ export default function AppNavbar() {
         <Navbar fluid rounded>
             <NavbarBrand href="https://flowbite-react.com">
                 <img src={reactLogo} className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">ToDo List React</span>
             </NavbarBrand>
             <div className="flex md:order-2">
                 {/*<Button onClick={toggleSwitchTheme} >Get started</Button>*/}
@@ -21,13 +21,13 @@ export default function AppNavbar() {
 
 
 
-                <NavbarLink href="#" active>
+                {/*<NavbarLink href="#" active>
                     Home
                 </NavbarLink>
                 <NavbarLink href="#">About</NavbarLink>
                 <NavbarLink href="#">Services</NavbarLink>
                 <NavbarLink href="#">Pricing</NavbarLink>
-                <NavbarLink href="#">Contact</NavbarLink>
+                <NavbarLink href="#">Contact</NavbarLink>*/}
             </NavbarCollapse>
         </Navbar>
     )
